0f511c195f0414742e766746228fd1e96763412b
AleksandarNesovic <crazynorth203@gmail.com>
2018-09-27 13:03:47 +0200

    first commit

---
+++ src/main/java/com/telnet/onlineshop/domain/Address.java
@@ -0,0 +1,133 @@
+package com.telnet.onlineshop.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+
+import javax.persistence.*;
+import javax.validation.constraints.*;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ * A Address.
+ */
+@Entity
+@Table(name = "address")
+@Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
+public class Address implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sequenceGenerator")
+    @SequenceGenerator(name = "sequenceGenerator")
+    private Long id;
+
+    @NotNull
+    @Column(name = "address", nullable = false)
+    private String address;
+
+    @Column(name = "city")
+    private String city;
+
+    @Column(name = "country")
+    private String country;
+
+    @ManyToOne
+    @JsonIgnoreProperties("addresses")
+    private Customer customer;
+
+    // jhipster-needle-entity-add-field - JHipster will add fields here, do not remove
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public Address address(String address) {
+        this.address = address;
+        return this;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public Address city(String city) {
+        this.city = city;
+        return this;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public Address country(String country) {
+        this.country = country;
+        return this;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public Address customer(Customer customer) {
+        this.customer = customer;
+        return this;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+    // jhipster-needle-entity-add-getters-setters - JHipster will add getters and setters here, do not remove
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Address address = (Address) o;
+        if (address.getId() == null || getId() == null) {
+            return false;
+        }
+        return Objects.equals(getId(), address.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(getId());
+    }
+
+    @Override
+    public String toString() {
+        return "Address{" +
+            "id=" + getId() +
+            ", address='" + getAddress() + "'" +
+            ", city='" + getCity() + "'" +
+            ", country='" + getCountry() + "'" +
+            "}";
+    }
+}
---
+++ src/main/java/com/telnet/onlineshop/domain/enumeration/CategoryStatus.java
@@ -0,0 +1,8 @@
+package com.telnet.onlineshop.domain.enumeration;
+
+/**
+ * The CategoryStatus enumeration.
+ */
+public enum CategoryStatus {
+    AVAILABLE, RESTRICTED, DISABLED
+}
